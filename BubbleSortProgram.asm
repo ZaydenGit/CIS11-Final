32e68fa2181c965ba910ad7adcf6ff2ae5f7b181
ZaydenGit <zaydenmiddleton12@gmail.com>
2025-06-08 22:41:29 -0700

    Baseline for asm file

--- BubbleSortProgram.asm
+++ BubbleSortProgram.asm
@@ -2,9 +2,16 @@
 
 ; MAIN PROGRAM
 
-ADD R0, R0, #0
-
-
+JSR GET_INPUTS
+JSR BUBBLE_SORT
+JSR DISPLAY_OUTPUT
 HALT
+
+GET_INPUTS	; subroutine to obtain inputs
+
+BUBBLE_SORT	; subroutine for bubble sort
+
+DISPLAY_OUTPUT	; subroutine to output sorted array
+
 
 .END
